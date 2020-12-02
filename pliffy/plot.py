0bc080bbd21f23b391cee788de6742e3f5f98cae
Martin Heroux <heroux.martin@gmail.com>
2020-12-02 14:53:07 +1100

    Minor bug fix to plot.py

--- pliffy/plot.py
+++ pliffy/plot.py
@@ -1,4 +1,4 @@
-from pliffy import estimate, parser, utils, figure
+import pliffy
 
 
 def plot_abd(info: "utils.PliffyInfoABD", ax=None):
@@ -11,8 +11,8 @@ def plot_abd(info: "utils.PliffyInfoABD", ax=None):
     >>> info = PliffyInfoABD(data_a=data_a, data_b=data_b)
     >>> plot_abd(info)
     """
-    estimates = estimate.calc_abd(info)
-    save, ab_info, diff_info = parser.abd(info, estimates)
-    ab_ax = figure.FigureAB(ab_info, ax)
-    diff_ax = figure.DiffAxCreator(ab_ax, info, diff_info).diff_ax()
-    figure.FigureDiff(diff_info, diff_ax, save)
+    estimates = pliffy.estimate.calc_abd(info)
+    save, ab_info, diff_info = pliffy.parser.abd(info, estimates)
+    ab_ax = pliffy.figure.FigureAB(ab_info, ax)
+    diff_ax = pliffy.figure.DiffAxCreator(ab_ax, info, diff_info).diff_ax()
+    pliffy.figure.FigureDiff(diff_info, diff_ax, save)
