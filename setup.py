89e20b8d2d3508eeaec05c6545f1347dd1733c13
Martin Heroux <heroux.martin@gmail.com>
2020-12-04 17:12:27 +1100

    Fix setup.py to include figure/ sub-package

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 import pathlib
 from setuptools import setup
+from setuptools import find_packages
 
 # The directory containing this file
 HERE = pathlib.Path(__file__).parent
@@ -10,7 +11,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="pliffy",
-    version="0.1.2",
+    version="0.1.3",
     description="Plotting differences with Python",
     long_description=README,
     long_description_content_type="text/markdown",
@@ -22,7 +23,7 @@ setup(
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Programming Language :: Python :: 3.8",
     ],
-    packages=["pliffy"],
+    packages=find_packages(),
     include_package_data=False,
     install_requires=["numpy>=1.19.1", "scipy>=1.5.2", "matplotlib>=3.3.1"],
     tests_require=["pytest>=6.0.1", "pytest-cov>=2.10.1", "pytest-mpl>= 0.11"]
